9058c0133dd377a5fe17c437c98efab0dba9ef7f
Hiago <hiagobarreiros96@gmail.com>
2022-07-04 22:52:58 -0300

    Preparando o ambiente, implementado HotReload.

---
+++ Switch/main.py
@@ -0,0 +1,33 @@
+import os
+from kivymd.app import MDApp
+from kaki.app import App
+from kivy.factory import Factory
+
+class LiveApp(MDApp, App):
+    """ Hi Windows users """
+
+    DEBUG = 1 # set this to 0 make live app not working
+
+    # *.kv files to watch
+    KV_FILES = {
+        os.path.join(os.getcwd(), "screens/screenmanager.kv"),
+        os.path.join(os.getcwd(), "screens/views/troca.kv"),
+    }
+
+    # class to watch from *.py files
+    CLASSES = {
+        "MainScreenManager": "screens.screenmanager",
+        "Troca": "screens.views.example",
+    }
+
+    # auto reload path
+    AUTORELOADER_PATHS = [
+        (".", {"recursive": True}),
+    ]
+
+    def build_app(self):
+
+        return Factory.MainScreenManager()
+
+if __name__ == "__main__":
+    LiveApp().run()
